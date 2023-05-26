dd7d93125c298fbd3799edf99fe5346f0797ad0e
CaO2323 <132775838+CaO2323@users.noreply.github.com>
2023-05-26 21:59:10 +0200

    Update Triangle.php
    Aquí no voy a usar los getters por eso los he suprimido,  es la forma que tenía de hacerlo con java.

--- Triangle.php
+++ Triangle.php
@@ -6,14 +6,7 @@ class Triangle extends Shape {
     public function __construct($width, $height) {
         $this->width = $width;
         $this->height = $height;
-    }
-    //Getters;
-    public function getWidth(){
-        return $this-> width;
-    }
-    public function getHeight(){
-        return $this -> height;
-    }   
+    } 
     //Triangle's own function;
     public function Area (){
         return 0.5 *($this -> width * $this -> height);
